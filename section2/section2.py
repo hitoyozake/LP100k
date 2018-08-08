c24e968429e9db2bdb3f876d383d6c83b3b550ea
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-08-08 14:20:27 +0900

    p14

--- section2/section2.py
+++ section2/section2.py
@@ -77,6 +77,23 @@ def problem_13():
 
     return out
 
+def problem_14(n):
+    """
+    14. 先頭からN行を出力
+自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
+    :param n:
+    :return:
+
+    """
+    with open(INPUT) as fp:
+        for i in range(n):
+            line = fp.readline()
+            line = line.replace('\n', '')
+            line = line.replace('\r', '')
+            print(line)
+            if line is None:
+                break
+
 if __name__ == "__main__":
     print("section2")
-    problem_13()
+    problem_14(4)
