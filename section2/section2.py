e6165bb2fef6783c7ac423ca1e8b99af888f92a2
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-08-08 09:31:18 +0900

    p11

--- section2/section2.py
+++ section2/section2.py
@@ -9,7 +9,6 @@ def problem_10():
 行数をカウントせよ．確認にはwcコマンドを用いよ．
     :return:
     """
-
     count = 0
 
     with open(INPUT) as fp:
@@ -20,6 +19,31 @@ def problem_10():
 
     return count
 
+def problem_11():
+    """
+
+    :return:
+    """
+    with open(INPUT) as fp:
+
+        strs = fp.readlines()
+        out = []
+        for i in strs:
+            out.append(i.replace('\t', ' '))
+
+        out = '\n'.join(out)
+
+        print(out)
+
+        return out
+
+
+    out = input.replace("\t", " ")
+    print(out)
+
+    return out
+
+
 if __name__ == "__main__":
     print("section2")
-    problem_10()
+    problem_11()
