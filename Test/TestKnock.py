78420dbd9ec559960ec73c95d42c62f05e8ca99a
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-08-08 09:33:01 +0900

    bash added

--- Test/TestKnock.py
+++ Test/TestKnock.py
@@ -2,6 +2,7 @@
 
 import unittest
 import section1
+from section2 import section2
 
 class TestKnock(unittest.TestCase):
 
@@ -10,3 +11,5 @@ class TestKnock(unittest.TestCase):
         self.assertEqual(section1.problem_01(), "パトカー")
         self.assertEqual(section1.problem_02(), "パタトクカシーー")
         self.assertEqual(section1.problem_07(1, 2, 3), '1時の2は3')
+
+        # self.assertEqual(section2.problem_10(), 24)
