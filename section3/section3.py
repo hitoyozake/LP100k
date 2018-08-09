bb0a794ecfc57a0ecc2f92d94a02fb2987502af5
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-08-09 20:48:15 +0900

    20

--- section3/section3.py
+++ section3/section3.py
@@ -1,2 +1,41 @@
 # -* encoding:utf-8 *-
+import json
 
+
+
+
+def load_json():
+    data = None
+    with open("jawiki-country.json", encoding='utf-8') as fp:
+        data = []
+        for i in fp:
+            data.append(json.loads(i))
+
+    return data
+
+def extract_about_UK(ls):
+    d = []
+
+    for i in ls:
+        if 'イギリス' in i['text'] or 'イギリス' in i['title']:
+            d.append(i)
+
+    return d
+
+
+def problem_20():
+    """
+    20. JSONデータの読み込み
+    Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．問題21-29では，ここで抽出した記事本文に対して実行せよ．
+    :return:
+    """
+    d = load_json()
+
+    d = extract_about_UK(d)
+
+    for i in d:
+        print(i)
+
+
+if __name__ == '__main__':
+    problem_20()
