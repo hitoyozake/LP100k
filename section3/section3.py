88eeaacf8560fe7cce5b066a144078a78d4b76e8
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-08-09 22:05:10 +0900

    22

--- section3/section3.py
+++ section3/section3.py
@@ -1,8 +1,7 @@
 # -* encoding:utf-8 *-
+# 正規表現
 import json
-
-
-
+import re
 
 def load_json():
     data = None
@@ -39,7 +38,7 @@ def problem_20():
 def problem_21():
     """
     21. カテゴリ名を含む行を抽出
-記事中でカテゴリ名を宣言している行を抽出せよ．
+    記事中でカテゴリ名を宣言している行を抽出せよ．
     :return:
     """
     d = load_json()
@@ -54,6 +53,19 @@ def problem_21():
                 print(j)
         print("******************")
 
+def problem_22():
+    """
+    22. カテゴリ名の抽出
+    記事のカテゴリ名を（行単位ではなく名前で）抽出せよ．
+    :return:
+    """
+    d = extract_about_UK(load_json())
+
+    for i in d:
+        x = i['text'].split('\n')
+        for j in x:
+            if re.match('.+\[Category:.+', j):
+                print(j)
 
 if __name__ == '__main__':
-    problem_21()
+    problem_22()
