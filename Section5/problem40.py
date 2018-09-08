514b38c9948af414d92e53b0729959386ed47115
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-09-08 17:00:00 +0900

    46. 47は46で抽出した"格とかかり先の動詞がサ変かどうかを見れば解けるはず"

--- Section5/problem40.py
+++ Section5/problem40.py
@@ -286,14 +286,46 @@ def problem_46():
 
 
     for a, b in zip(v_dict.items(), v_dict2.items()):
-        print("{0}: {1}, {2}".format(a[0], '\t'.join(a[1]), '\t'.join(b[1])))
+        print("{0}: {1}, {2}".format(a[0], ' '.join(a[1]), ' '.join(b[1])))
 
 
 def problem_47():
     """
+47. 機能動詞構文のマイニング
+動詞のヲ格にサ変接続名詞が入っている場合のみに着目したい．46のプログラムを以下の仕様を満たすように改変せよ．
 
+「サ変接続名詞+を（助詞）」で構成される文節が動詞に係る場合のみを対象とする
+述語は「サ変接続名詞+を+動詞の基本形」とし，文節中に複数の動詞があるときは，最左の動詞を用いる
+述語に係る助詞（文節）が複数あるときは，すべての助詞をスペース区切りで辞書順に並べる
+述語に係る文節が複数ある場合は，すべての項をスペース区切りで並べる（助詞の並び順と揃えよ）
+例えば「別段くるにも及ばんさと、主人は手紙に返事をする。」という文から，以下の出力が得られるはずである．
+
+返事をする      と に は        及ばんさと 手紙に 主人は
+このプログラムの出力をファイルに保存し，以下の事項をUNIXコマンドを用いて確認せよ．
+
+コーパス中で頻出する述語（サ変接続名詞+を+動詞）
+コーパス中で頻出する述語と助詞パターン
     :return:
     """
+    sentences = problem_41()
+
+    sentence = sentences[7]
+
+    v_dict = defaultdict(lambda: [])
+    v_dict2 = defaultdict(lambda: [])
+
+    for chunk in sentence:
+        for morph in chunk.morphs:
+            if morph.pos == '助詞':
+                if chunk.dst != -1:
+                    for mrp in sentence[chunk.dst].morphs:
+                        if mrp.pos == '動詞':
+                            v_dict[mrp.base].append(morph.surface)
+                            v_dict2[mrp.base].append(''.join([m.surface for m in chunk.morphs]))
+
+
+    for a, b in zip(v_dict.items(), v_dict2.items()):
+        print("{0}: {1}, {2}".format(a[0], ' '.join(a[1]), ' '.join(b[1])))
 
 
 
